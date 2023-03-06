90466a30ca7a09aae7ce2c980a5a6347d217232f
John Marcel Silveira <marcel-eu@hotmail.com>
2023-03-06 15:53:59 -0300

    ex04

--- aula2Ex4.java
+++ aula2Ex4.java
@@ -1,7 +1,7 @@
 public class aula2Ex4 {
 
     public static void main(String[] args) {
-        int vet[] = { 1, 1, 1, 1, 1, 2, 1, 2, 1, 2 }, atual = 0;
+        int vet[] = { 1, 1, 1, 1, 1, 2, 1, 2, 1, 3,3 }, atual = 0;
         int vet2[] = new int[vet.length];
 
         boolean achei = false;
@@ -9,25 +9,27 @@ public class aula2Ex4 {
             for (int j = i + 1; j < vet.length; j++) {
                 if (vet[i] == vet[j]) {
                     for (int k = 0; k <= atual; k++) {
-                        //preciso percorrer o vetor auxiliar e só incluir o número se não achar dentro do vetor
-                       if(vet2[k]==vet[i])
-                        achei = true;
-                        break;
+                        // preciso percorrer o vetor auxiliar e só incluir o número se não achar dentro
+                        // do vetor
+                        if (vet2[k] == vet[j]) {
+                            achei = true;
+                            break;
+                        }
                     }
-                    if(!achei)
-                    vet2[atual] = vet[i];
+                    if (!achei) {
+                        vet2[atual] = vet[i];
+                        atual++;
+                    }
+                    break;
                 }
-                //achei = true;
-                atual++;
-                break;
             }
             achei = false;
         }
         System.out.println();
 
         for (int i = 0; i < atual; i++) {
-            if(vet2[i]>0)
-            System.out.print(vet2[i] + " ");
+            if (vet2[i] > 0)
+                System.out.print(vet2[i] + " ");
         }
     }
 }
